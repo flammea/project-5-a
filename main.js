510cd0eb6e63c918a2786f0edbdd700aefaf4cb1
Beata Markunas <beata.markunas@gmail.com>
2019-01-06 20:56:38 +0100

    Sett CSS Styles using JS.

--- main.js
+++ main.js
@@ -11,3 +11,11 @@ input.addEventListener("input", (e) => {
         div.textContent = "";
     }
 })
+
+input.addEventListener("focus", (e) => {
+    e.target.classList.add("active");
+})
+
+input.addEventListener("blur", (e) => {
+    e.target.classList.remove("active");
+})
