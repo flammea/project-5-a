126be385299e9b11e981fab621a8662854d46ee2
Beata Markunas <beata.markunas@gmail.com>
2019-01-06 20:22:40 +0100

    Improve if else statement.

--- main.js
+++ main.js
@@ -6,5 +6,7 @@ const message = "Dobrze się spisałeś";
 input.addEventListener("input", (e) => {
     if (password === e.target.value) {
         div.textContent = message;
+    } else {
+        div.textContent = "";
     }
 })
